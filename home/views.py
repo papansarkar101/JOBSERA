0502ddd1c438b57d0e859a5695a58457d1f11432
Papan Sarkar <36565033+papansarkar101@users.noreply.github.com>
2021-01-02 12:31:48 +0530

    updates

--- home/views.py
+++ home/views.py
@@ -1,4 +1,11 @@
+import requests
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.urls import reverse_lazy, reverse
+from bs4 import BeautifulSoup
+from requests.compat import quote_plus
+from . import models
+
 
 # Create your views here.
 def home(request):
@@ -6,4 +13,10 @@ def home(request):
 
 
 def search(request):
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+
+    send_searches_to_frontend = {
+        'search': search,
+    }
     return render(request, 'search.html')
