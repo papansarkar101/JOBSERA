20a7c821c943bc10e20ca70aebf527a5c037069b
Papan Sarkar <36565033+papansarkar101@users.noreply.github.com>
2021-01-02 12:09:52 +0530

    updates

--- jobsera/settings.py
+++ jobsera/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'jobsera.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
