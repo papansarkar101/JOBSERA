7d605b68da147cabcf2306fc48b8723f8bb031c8
Papan Sarkar <36565033+papansarkar101@users.noreply.github.com>
2021-01-02 11:38:55 +0530

    setting

--- jobsera/settings.py
+++ jobsera/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
@@ -117,12 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = (BASE_DIR, 'staticfiles')
+# STATIC_ROOT = (BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 django_heroku.settings(locals())
