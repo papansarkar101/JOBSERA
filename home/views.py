29e92aa0183bfcbfe4072e0b7bcc0c1e8641817b
Papan Sarkar <36565033+papansarkar101@users.noreply.github.com>
2021-01-02 14:09:14 +0530

    app

--- home/views.py
+++ home/views.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 from requests.compat import quote_plus
 from . import models
 
+BASE_URL = "https://in.indeed.com/jobs?q={}"
+
 
 # Create your views here.
 def home(request):
@@ -16,7 +18,28 @@ def search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
 
+    final_url = BASE_URL.format(quote_plus(search))
+
+    response = requests.get(final_url)  # <Response [200]>
+    data = response.text  # Everything is in data now
+
+    soup = BeautifulSoup(data, features='html.parser')
+
+    all_post = soup.find_all(
+        'div', {'class': 'jobsearch-SerpJobCard'})  # All job posts
+
+    final_posts = []
+
+    for post in all_post:
+        post_title = post.find(class_="title").text
+        company = post.find(class_="company").text
+        location = post.find(class_="location").text
+
+        final_posts.append((post_title, company, location))
+
     send_searches_to_frontend = {
-        'search': search,
+        'final_url': final_url,
+        'search': search.title(),
+        'final_posts': final_posts,
     }
-    return render(request, 'search.html')
+    return render(request, 'search.html', send_searches_to_frontend)
